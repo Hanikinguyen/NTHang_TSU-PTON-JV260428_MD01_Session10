2a55e94ada50f6e95fd04ec7059a7b644c65d500
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-06-16 08:06:36 +0700

    Upload bài tập 03 của SS10

---
+++ Ex03/Ex03.js
@@ -0,0 +1,4 @@
+let width = Number(prompt('Nhập vào chiều dài của hình chữ nhật: '));
+let height = Number(prompt('Nhập vào chiều rộng của hình chữ nhật: '));
+
+console.log('Diện tích là: ' + width * height);
